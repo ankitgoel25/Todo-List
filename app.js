c6dc7a49e5218a37f4232ef05d120673c019b3ef
Ankit Goel <ankitoct01@gmail.com>
2020-09-17 22:22:43 +0530

    Todo List v1

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const date = require(__dirname + '/date.js');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const items = ["First", "Second", "Third"];
+const workItems = [];
+
+app.get("/", (req, res) => {
+    let day = date.getDate();
+    res.render("list", { listTitle: day, newListItems: items });
+});
+
+app.post("/", (req, res) => {
+    let item = req.body.newItem;
+    let list = req.body.list;
+
+    if (list === "Work List") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+});
+
+app.get("/work", (req, res) => {
+    res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+})
---
+++ date.js
@@ -0,0 +1,17 @@
+const today = new Date();
+
+exports.getDate = function () {
+    const options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+    return today.toLocaleDateString("en-US", options);
+}
+
+exports.getDay = function () {
+    const options = {
+        weekday: "long"
+    };
+    return today.toLocaleDateString("en-US", options);
+}
